eac8bd06fd686b19041e4997f7681eda6f329a30
Alex <Alexkopylov123@gmail.com>
2024-01-29 21:12:14 +0400

    bugfix for poll author check

--- src/handlers/game.py
+++ src/handlers/game.py
@@ -34,6 +34,7 @@ async def game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             "chat_id": update.effective_chat.id,
             "chat_name": update.effective_chat.title,
             "creator_id": update.effective_user.id,
+            "creator_username": update.effective_user.username,
             "results": {},
         }
     }
@@ -59,34 +60,40 @@ async def save(
         await update.effective_message.reply_text(
             "You will record result by replying to my poll. Have you started the game by /game command?"
         )
+        return
     # check reply msg is from group chat
     if not is_message_from_group_chat(msg_with_poll):
         await update.effective_message.reply_text(
             "You can only save game in a group chat where other players can see the results."
         )
+        return
     # check reply msg is a poll
     if not message_is_poll(msg_with_poll):
         await update.effective_message.reply_text(
             "Please reply to a poll to stop and record results."
         )
+        return
     # check reply msg is from the bot
     if msg_with_poll.from_user.id != context.bot.id:
         await update.effective_message.reply_text(
             f"Please reply to poll created by me @{context.bot.username}."
         )
+        return
     # check user is creator of the poll
     if (
         update.effective_user.id
         != context.bot_data[msg_with_poll.poll.id]["creator_id"]
     ):
         await update.effective_message.reply_text(
-            f"You are not the creator of the game!"
-            f"Only {context.bot_data[update.poll.id]['creator_id']} can stop this poll."
+            f"You are not the creator of the game! "
+            f"Only @{context.bot_data[msg_with_poll.poll.id]['creator_username']} can stop this poll."
         )
+        return
     if update.effective_chat.id != context.bot_data[msg_with_poll.poll.id]["chat_id"]:
         await update.effective_message.reply_text(
             f"You can only save game in a group chat where other players can see the results."
         )
+        return
 
     await context.bot.stop_poll(update.effective_chat.id, msg_with_poll.id)
     poll_results = context.bot_data[msg_with_poll.poll.id]["results"]
