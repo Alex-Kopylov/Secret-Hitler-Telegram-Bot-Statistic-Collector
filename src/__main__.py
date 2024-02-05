19ac49a1792b91597fdb3673eb69378f1b06392f
Alex <Alexkopylov123@gmail.com>
2024-02-05 14:57:45 +0400

    errors handler

--- src/__main__.py
+++ src/__main__.py
@@ -2,43 +2,25 @@
 Telegram bot entry point.
 
 """
-
 import logging
-from typing import Type
+import traceback
 
 from telegram import (
     Update,
 )
 from telegram.ext import (
     Application,
-    CommandHandler,
-    PollAnswerHandler,
 )
-from telegram.ext import BaseHandler
 
 from src import config
 from src import handlers
-from src.callbacks.receive_poll_answer import receive_poll_answer
-from src.db import close_db, get_db
+from src.db import close_db
+from src.get_handlers import get_handlers
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-logger = logging.getLogger(__name__)
-
-
-def get_handlers() -> list:
-    commands = [
-        CommandHandler("start", handlers.start),
-        CommandHandler("help", handlers.help),
-        CommandHandler("game", handlers.game),
-        CommandHandler("save", handlers.save),
-    ]
-    poll_answer_handler = [
-        PollAnswerHandler(receive_poll_answer),
-    ]
-
-    return commands + poll_answer_handler
+logger = logging.getLogger()
 
 
 def main() -> None:
@@ -47,7 +29,9 @@ def main() -> None:
 
     application = Application.builder().token(config.TELEGRAM_BOT_TOKEN).build()
     application.add_handlers(get_handlers())
-
+    application.add_error_handler(handlers.error_handler)
+    # send a message to the developer when the bot is ready
+    application.add_handlers()
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
@@ -55,8 +39,6 @@ if __name__ == "__main__":
     try:
         main()
     except Exception:
-        import traceback
-
         logger.warning(traceback.format_exc())
     finally:
         close_db()
