b57d6ec430ab9454d9f1c9a2dbdb55b0e0203e39
Alex <Alexkopylov123@gmail.com>
2024-02-07 03:14:25 +0400

    hitler winner and spectator poll options (#7)
    * hitler winner and spectator poll options

--- src/data_models/Record.py
+++ src/data_models/Record.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Literal
+from typing import Literal, Optional
 from src import config
 from pydantic import BaseModel, field_validator
 
@@ -13,12 +13,16 @@ class Record(BaseModel):
 
     @field_validator("role", mode="after")
     @classmethod
-    def shorten_role(cls, v: str) -> Literal["CH", "DH", "HL", "LW", "LL", "FW", "FL"]:
+    def shorten_role(
+        cls, v: str
+    ) -> Optional[Literal["CH", "DH", "HW", "HL", "LW", "LL", "FW", "FL"] | None]:
         match v:
             case "I'm Canceler Hitler":
                 return "CH"
             case "I'm Dead Hitler":
                 return "DH"
+            case "I'm Hitler Winner":
+                return "HW"
             case "I'm Liberal Winner":
                 return "LW"
             case "I'm Hitler Loser":
@@ -29,6 +33,8 @@ class Record(BaseModel):
                 return "FW"
             case "I'm Fascistic Loser":
                 return "FL"
+            case "👀 SPECTATOR | NOT A PLAYER 👀":
+                return None
             case _:
                 raise ValueError(
                     f"Invalid role '{v}' for Record. Role must be one of {config.GAME_POLL_OUTCOMES}"
--- src/services/db_service.py
+++ src/services/db_service.py
@@ -1,6 +1,12 @@
 import logging
 import sqlite3
 
+from src.data_models.Playroom import Playroom
+import logging
+import sqlite3
+
+from src.data_models.Game import Game
+from src.data_models.Player import Player
 from src.data_models.Game import Game
 from src.data_models.Player import Player
 from src.data_models.Playroom import Playroom
@@ -9,17 +15,33 @@ from src.db import execute
 
 
 async def save_record(record: Record) -> None:
-    await execute(
-        """INSERT INTO records (creator_id, player_id, playroom_id, game_id, role)
-        VALUES (?, ?, ?, ?, ?)""",
-        (
-            record.creator_id,
-            record.player_id,
-            record.playroom_id,
-            record.game_id,
-            record.role,
-        ),
-    )
+    try:
+        await execute(
+            """INSERT INTO records (creator_id, player_id, playroom_id, game_id, role)
+            VALUES (?, ?, ?, ?, ?)""",
+            (
+                record.creator_id,
+                record.player_id,
+                record.playroom_id,
+                record.game_id,
+                record.role,
+            ),
+        )
+    except sqlite3.IntegrityError:
+        logging.info(
+            f"Something went wrong with game: {record.game_id} in playroom {record.playroom_id}"
+        )
+
+
+async def save_playroom(playroom: Playroom) -> None:
+    """Add a game room to the bot_data"""
+    try:
+        await execute(
+            "INSERT INTO playrooms (id, name) VALUES (?, ?)",
+            (playroom.telegram_chat_id, playroom.name),
+        )
+    except sqlite3.IntegrityError:
+        logging.info(f"Playroom {playroom.name} already exists in the database")
 
 
 async def save_playroom(playroom: Playroom) -> None:
